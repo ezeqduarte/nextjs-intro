30b9ad77edd5192c0c6d0b9b26d9e00b0670e57c
ezeqduarte <ezeqduarte01@gmail.com>
2024-03-16 22:13:14 -0300

    cap 9: Partial prerendering

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,9 +2,9 @@ import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { CardsSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
   const {
@@ -27,9 +27,7 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart/>
         </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
-        </Suspense>
+        <LatestInvoices />
       </div>
     </main>
   );
